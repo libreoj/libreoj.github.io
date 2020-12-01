6a4558d73420a21876501c611c9bae194f4a381b
Menci‮ <huanghaorui301@gmail.com>
2020-12-01 23:35:42 +0800

    Fix click on <a>'s child <img>

--- redirect.js
+++ redirect.js
@@ -1,9 +1,11 @@
 window.addEventListener('click', function (e) {
-	if (e.target.tagName === 'A') {
-		var path = e.target.getAttribute('href');
-		if (path.indexOf('//') === -1 && !path.startsWith('/contest')) {
-			e.preventDefault();
-			location = 'https://loj.ac' + path;
+	for (var elem = e.target; elem; elem = elem.parentElement)
+		if (elem.tagName === 'A') {
+			var path = elem.getAttribute('href');
+			if (path.indexOf('//') === -1 && !path.startsWith('/contest')) {
+				e.preventDefault();
+				location = 'https://loj.ac' + path;
+				return;
+			}
 		}
-	}
 })
